c1c85d4ec1aa6415b0c4a58d6403586e2bc27da2
anthonymnf <anthonymnf30@gmail.com>
2024-02-03 10:48:52 -0300

    Tailwind config e estilizações iniciais

--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -1,7 +1,40 @@
 export default function Form() {
   return (
-    <>
-      <h1>Inscreva-se</h1>{" "}
-    </>
+    <form className="flex flex-col gap-3 flex-wrap">
+      <div className="flex flex-col">
+        <label className="text-xs" htmlFor="Name">
+          Nome:
+        </label>
+        <input
+          type="text"
+          name="Name"
+          placeholder="Digite o seu nome"
+          className="rounded-lg py-2 px-2 text-sm placeholder:text-sm placeholder:text-stone-400"
+        />
+      </div>
+      <div className="flex flex-col">
+        <label className="text-xs" htmlFor="email">
+          E-mail:
+        </label>
+        <input
+          type="email"
+          name="email"
+          placeholder="Digite o seu email"
+          className="rounded-lg py-2 px-2 text-sm placeholder:text-sm placeholder:text-stone-400"
+        />
+      </div>
+      <div className="flex flex-col">
+        <a href="#" className="text-xs underline mb-2">
+          Leia os termos
+        </a>
+        <div>
+          <input type="checkbox" />
+          <label className="text-xs" htmlFor="agree">
+            Concordo com os termos
+          </label>
+        </div>
+      </div>
+      <button>Cadastrar</button>
+    </form>
   );
 }
