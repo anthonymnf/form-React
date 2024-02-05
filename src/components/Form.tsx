38bcc2114ccd9aace803b5199604e48c10ff81cf
anthonymnf <anthonymnf30@gmail.com>
2024-02-05 09:53:41 -0300

    Validação do formulário

--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -1,38 +1,87 @@
+import { useState, FormEvent } from "react";
+import { User } from "../types/User";
+import { validate } from "../utils/validate";
+
 export default function Form() {
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [agree, setAgree] = useState(false);
+  const [errors, setErrors] = useState<User | null>(null);
+
+  const handleSubmit = (ev: FormEvent) => {
+    ev.preventDefault();
+    setErrors(null);
+
+    const data: User = {
+      name,
+      email,
+      agree,
+    };
+
+    const validateErrors = validate(data);
+
+    if (Object.keys(validateErrors).length > 0) {
+      setErrors(validateErrors);
+      return;
+    }
+    alert("Obrigado por se inscrever!!");
+
+    setName("");
+    setEmail("");
+    setAgree(false);
+  };
+
   return (
-    <form className="flex flex-col gap-3 flex-wrap">
+    <form className="flex flex-col gap-3 flex-wrap" onSubmit={handleSubmit}>
       <div className="flex flex-col">
         <label className="text-sm" htmlFor="Name">
           Nome:
         </label>
         <input
+          value={name}
+          onChange={(ev) => setName(ev.target.value)}
           type="text"
           name="Name"
           placeholder="Digite o seu nome"
           className="rounded-lg py-2 px-2 text-sm placeholder:text-sm placeholder:text-stone-400"
         />
+        {errors?.name && (
+          <small className="text-xs text-red-500 mt-1">{errors.name}</small>
+        )}
       </div>
       <div className="flex flex-col">
         <label className="text-sm" htmlFor="email">
           E-mail:
         </label>
         <input
+          value={email}
+          onChange={(ev) => setEmail(ev.target.value)}
           type="email"
           name="email"
           placeholder="Digite o seu email"
           className="rounded-lg py-2 px-2 text-sm placeholder:text-sm placeholder:text-stone-400"
         />
+        {errors?.email && (
+          <small className="text-xs text-red-500 mt-1">{errors.email}</small>
+        )}
       </div>
       <div className="flex flex-col">
         <a href="#" className="text-sm underline mb-2">
           Leia os termos
         </a>
         <div className="flex gap-2 items-center">
-          <input type="checkbox" />
+          <input
+            type="checkbox"
+            checked={agree}
+            onChange={(ev) => setAgree(ev.target.checked)}
+          />
           <label className="text-sm" htmlFor="agree">
             Concordo com os termos
           </label>
         </div>
+        {errors?.agree && (
+          <small className="text-xs text-red-500 mt-1">{errors.agree}</small>
+        )}
       </div>
       <button
         type="submit"
